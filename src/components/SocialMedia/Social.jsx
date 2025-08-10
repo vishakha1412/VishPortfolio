d8126ffc3ca05bf0aa4045483f51da5ab7e06d5e
vishakha1412 <vish60617@gmail.com>
2025-08-10 15:35:35 +0530

    some changes

--- src/components/SocialMedia/Social.jsx
+++ src/components/SocialMedia/Social.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { NavLink } from 'react-router'
+import { NavLink, useNavigate } from 'react-router'
  
 
 
 export const Social = () => {
+  const navigate=useNavigate();
   return (
     <>
      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-indigo-600 via-purple-700 to-pink-600 px-6 text-white">
@@ -12,8 +13,11 @@ export const Social = () => {
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-center space-x-4">
           
             
-           <NavLink to= 'www.linkedin.com/in/vishakha-sharma-0a056032a' className='bg-white/10 shadow-white/20 rounded-2xl p-6'>LinkedIn</NavLink>
+           <NavLink to= 'https://www.linkedin.com/in/vishakha-sharma-0a056032a/?trk=public_profile-settings_edit-profile-content&originalSubdomain=in' className='bg-white/10 shadow-white/20 rounded-2xl p-6'>LinkedIn</NavLink>
            <NavLink className='bg-white/10 shadow-white/20 rounded-2xl p-6'>Intsagram</NavLink>
+           <NavLink className='bg-white/10 shadow-white/20 rounded-2xl p-6'>Github</NavLink>
+           <NavLink className='bg-white/10 shadow-white/20 rounded-2xl p-6'>Email</NavLink>
+           <NavLink  className='bg-white/10 shadow-white/20 rounded-2xl p-6'>Twitter</NavLink>
           
           
         </ul>
